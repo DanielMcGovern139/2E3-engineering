14a6dc781151789f3e7327261c41572f544a79ad
DanielMcGovern139 <96776714+DanielMcGovern139@users.noreply.github.com>
2021-12-29 19:33:10 +0000

    Create date.hpp

---
+++ Date-events-class/date.hpp
@@ -0,0 +1,20 @@
+#pragma once
+class date {
+  protected:
+  int year;
+  int month;
+  int day; // day of month
+  std::string weekday;
+  public:
+  date(int y, int m, int d); // convenience constructor
+  date(); //default constructor
+  ~date();
+
+  void print_diagnostics(); // diagnostic only -- show us the values in the date object
+
+  // getters and setters should be here
+
+  // methods we are required to implement
+  int get_date_number();
+  int get_day_of_week();
+};
